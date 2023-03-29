23b20a6833433266d005593c0f7278c8d3b9ed69
theo <theogenix@gmail.com>
2023-03-29 22:55:51 +0200

    ajout delete vehicle sur web

---
+++ RentManager2/rentmanager/src/main/java/com/epf/rentmanager/servlet/VehiculeDeleteServlet.java
@@ -0,0 +1,46 @@
+package com.epf.rentmanager.servlet;
+
+
+import com.epf.rentmanager.exception.ServiceException;
+import com.epf.rentmanager.model.Client;
+import com.epf.rentmanager.service.ClientService;
+import com.epf.rentmanager.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@WebServlet("/vehicles/delete")
+public class VehiculeDeleteServlet extends HttpServlet {
+    @Autowired
+    VehicleService vehicleService;
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        SpringBeanAutowiringSupport.processInjectionBasedOnCurrentContext(this);
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        this.getServletContext().getRequestDispatcher("/WEB-INF/views/vehicles/delete.jsp").forward(req,resp);
+    }
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long vehicle_id;
+        vehicle_id= Long.parseLong(req.getParameter("vehicle_id"));
+        try {
+            this.vehicleService.delete(vehicle_id);
+        } catch (ServiceException e) {
+            e.printStackTrace();
+        }
+        resp.sendRedirect("/rentmanager/cars");
+    }
+
+
+}
